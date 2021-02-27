a096b1125f7cc54ac1eb7f1ad882c075a26b4c0f
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-02-27 11:04:52 +0900

    feat: add user model

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  final String accountId;
+  final String name;
+  final String profileImage;
+
+  UserModel({
+    this.accountId,
+    this.name,
+    this.profileImage,
+  });
+}
