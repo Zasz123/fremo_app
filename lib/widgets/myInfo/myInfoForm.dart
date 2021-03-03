5e717d3472a193a0cd2b35e5b64a2d37410214ed
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-03-03 22:50:50 +0900

    feat: add user form widget

---
+++ lib/widgets/myInfo/myInfoForm.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import 'package:fremo_app/widgets/common/CustomFormWidget.dart';
+
+class MyInfoForm extends StatefulWidget {
+  @override
+  _MyInfoFormState createState() => _MyInfoFormState();
+}
+
+class _MyInfoFormState extends State<MyInfoForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            CustomInput(
+              placeholder: "아이디",
+              validatorFunction: (value) {
+                if (value.isEmpty) {
+                  return '아이디가 비었습니다.';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            CustomInput(
+              placeholder: "이름",
+              validatorFunction: (value) {
+                if (value.isEmpty) {
+                  return '이름이 비었습니다.';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            CustomInput(
+              placeholder: "이메일",
+              validatorFunction: (value) {
+                if (value.isEmpty) {
+                  return '이메일이 비었습니다.';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            SizedBox(
+              height: 40.0,
+              width: double.infinity,
+              child: RaisedButton(
+                elevation: 0,
+                color: Color(0xff95C7AE),
+                child: Text(
+                  "수정",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("data is valid!"),
+                      ),
+                    );
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
