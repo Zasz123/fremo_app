0d51392f22a2b232eebcdcf812b477c9d2bfe2c9
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-03-03 22:51:02 +0900

    fix: update myInfo screen

--- lib/pages/myInfo.dart
+++ lib/pages/myInfo.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:fremo_app/utils/googleAuth.dart';
+
+import 'package:fremo_app/widgets/myInfo/myInfoProfile.dart';
+import 'package:fremo_app/widgets/myInfo/myInfoForm.dart';
 
 class MyInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
-        child: Column(
-          children: [
-            RaisedButton(
-              child: Text("구글 로그인"),
-              onPressed: () async {
-                await googleLogin();
-              },
-            ),
-            RaisedButton(
-              child: Text("로그아웃"),
-              onPressed: () async {
-                await googleLogout();
-              },
-            )
-          ],
-        )
-        // child: MyInfoProfile(),
-        );
+      padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+      child: Column(
+        children: [
+          MyInfoProfile(),
+          SizedBox(
+            height: 20.0,
+          ),
+          MyInfoForm(),
+        ],
+      ),
+    );
   }
 }
+
+// Column(
+//           children: [
+//             Text(_userInfo == null ? "로그인 해주세요." : _userInfo.displayName),
+//             _userInfo == null
+//                 ? RaisedButton(
+//                     child: Text("구글 로그인"),
+//                     onPressed: () async {
+//                       User loggedInUser = await googleLogin();
+//                       _userProvider.setUser(loggedInUser);
+//                     },
+//                   )
+//                 : RaisedButton(
+//                     child: Text("로그아웃"),
+//                     onPressed: () async {
+//                       await googleLogout();
+//                       _userProvider.setUser(null);
+//                     },
+//                   ),
+//           ],
+//         )
