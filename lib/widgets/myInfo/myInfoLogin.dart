5e78d1c4138d2db53f38835fbb56bae0d9932ed0
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-03-04 22:25:50 +0900

    feat: add firebase login page

---
+++ lib/widgets/myInfo/myInfoLogin.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fremo_app/providers/user.dart';
+
+import 'package:fremo_app/utils/googleAuth.dart';
+import 'package:provider/provider.dart';
+
+class MyInfoLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    UserProvider _userProvider = Provider.of<UserProvider>(context);
+
+    return Column(
+      children: [
+        Text("Fremo!"),
+        SizedBox(
+          width: double.infinity,
+          child: RaisedButton(
+            child: Text("구글에 로그인"),
+            onPressed: () async {
+              User user = await googleLogin();
+              _userProvider.user = user;
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
