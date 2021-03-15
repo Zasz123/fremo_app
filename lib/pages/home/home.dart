b016f85db1771adeca90f94e530a1d17bf99f44d
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-03-15 21:22:35 +0900

    feat: update home screen widget

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:fremo_app/providers/memoProvider.dart';
+
+import 'package:fremo_app/pages/home/homeLanding.dart';
+import 'package:fremo_app/pages/home/homeArticle.dart';
 
 import 'package:fremo_app/widgets/common/CustomWidgetWrapper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    MemoProvider memoProvider = context.watch<MemoProvider>();
+
     return SingleChildScrollView(
       padding: EdgeInsets.symmetric(
         vertical: 5.0,
         horizontal: 10.0,
       ),
       child: CustomWidgetWrapper(
-        child: Text("home"),
+        child:
+            memoProvider.checkShowMemo ? HomeArticlePage() : HomeLandingPage(),
       ),
     );
   }
