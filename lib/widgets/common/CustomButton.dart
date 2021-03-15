0776e814c2a602e2256591e5a21ac65dead2dc2d
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-03-15 21:23:31 +0900

    feat: add outlined button

--- lib/widgets/common/CustomButton.dart
+++ lib/widgets/common/CustomButton.dart
@@ -2,20 +2,84 @@ import 'package:flutter/material.dart';
 
 class CustomDefaultButton extends StatelessWidget {
   final Widget child;
+  final String text;
   final Function onPressed;
+  final bool fullWidth;
 
-  CustomDefaultButton({this.child, this.onPressed});
+  CustomDefaultButton({
+    this.child,
+    this.text,
+    this.onPressed,
+    this.fullWidth = true,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5.0),
+    return SizedBox(
+      height: 40.0,
+      width: fullWidth ? double.infinity : null,
+      child: RaisedButton(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        color: Color(0xff95C7AE),
+        child: this.text != null
+            ? Text(
+                this.text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            : this.child,
+        onPressed: this.onPressed,
       ),
-      color: Color(0xff95C7AE),
-      child: this.child,
-      onPressed: this.onPressed,
+    );
+  }
+}
+
+class CustomOutlineButton extends StatelessWidget {
+  final Widget child;
+  final String text;
+  final Function onPressed;
+  final bool fullWidth;
+
+  CustomOutlineButton({
+    this.child,
+    this.text,
+    this.onPressed,
+    this.fullWidth = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40.0,
+      width: fullWidth ? double.infinity : null,
+      child: RaisedButton(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+          side: BorderSide(
+            color: Color(0xff95C7AE),
+            width: 2.5,
+          ),
+        ),
+        color: Colors.white,
+        child: this.text != null
+            ? Text(
+                this.text,
+                style: TextStyle(
+                  color: Color(0xff95C7AE),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            : this.child,
+        onPressed: this.onPressed,
+      ),
     );
   }
 }
