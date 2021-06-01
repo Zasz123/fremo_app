5e3fd3a3677bcd4a1b6a8d28748dd20b560be012
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-06-01 23:04:56 +0900

    feat: memo model add convert method

--- lib/models/memo.dart
+++ lib/models/memo.dart
@@ -1,6 +1,37 @@
-class Memo {
-  final String title;
-  final String description;
+import 'package:fremo_app/models/user.dart';
 
-  Memo({this.title, this.description});
+class Memo {
+  final int id;
+  final String title;
+  final String body;
+  final DateTime createdAt;
+  final User writer;
+
+  Memo({
+    this.id,
+    this.title,
+    this.body,
+    this.createdAt,
+    this.writer,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'title': this.title,
+      'body': this.body,
+      'createdAt': this.createdAt,
+      'writer': this.writer,
+    };
+  }
+
+  factory Memo.fromJson(Map<String, dynamic> json) {
+    return Memo(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      createdAt: DateTime.parse(json['createdAt']),
+      writer: User.fromJson(json['writer']),
+    );
+  }
 }
